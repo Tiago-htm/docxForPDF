ddb4691eed1cb1635038ce4f478ea4c5e8890af0
Tiago-htm <tiago.carvalho@gfinnovation.com.br>
2026-01-06 19:42:04 -0300

    Adicionado metodo para ler o arquivo e pegar suas palavras chaves e colocar em outro arquivo, obs: error é palavra reservada

--- main.go
+++ main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"archive/zip"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
+	"os"
+	"strings"
 )
 
+type Body struct {
+	Paragraphs []Paragraph `xml:"p"`
+}
+type Document struct {
+	XMLName xml.Name `xml:"document"`
+	Body    Body     `xml:"body"`
+}
+
+type Text struct {
+	Content string `xml:",chardata"`
+}
+
+type Paragraph struct {
+	Texts []Text `xml:"r>t"`
+}
+
 func main() {
-	r, error := zip.OpenReader("computa.docx")
-	if error != nil {
-		log.Fatal(error, `Error ao ler aquivo`)
+	r, err := zip.OpenReader("computa.docx")
+	if err != nil {
+		log.Fatal(err, `err ao ler aquivo`)
 
 	}
 
+	defer r.Close()
+
+	var todasPalavras []string
+
 	for _, f := range r.File {
 		if f.Name == "word/document.xml" {
-			rc, error := f.Open()
-			if error != nil {
-				log.Fatal(error)
+			rc, err := f.Open()
+			if err != nil {
+				log.Fatal(err)
 			}
 
 			content, err := io.ReadAll(rc)
@@ -27,9 +50,38 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
+			var doc Document
+			err = xml.Unmarshal(content, &doc)
 
-			fmt.Println(string(content))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, para := range doc.Body.Paragraphs {
+				for _, text := range para.Texts {
+					palavras := strings.Fields(text.Content)
+					todasPalavras = append(todasPalavras, palavras...)
+				}
+			}
+
 		}
 	}
+
+	arquivo, err := os.Create("palavras.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer arquivo.Close()
+
+	for _, palavra := range todasPalavras {
+		_, err := arquivo.WriteString(palavra + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
+	fmt.Println("Escrita terminada....")
 
 }
