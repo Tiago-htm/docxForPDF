984d7d11162a7c5ed57d65ba0411fe77d0c97651
Tiago-htm <tiago.carvalho@gfinnovation.com.br>
2026-01-07 22:58:29 -0300

    feat - adicionado limpeza de palavras

--- main.go
+++ main.go
@@ -6,17 +6,22 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"path/filepath"
 
 	//externas
 	"github.com/jung-kurt/gofpdf"
 )
 
 func main() {
+
 	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	pdf.SetFont("Arial", "", 12)
+
+	tr := pdf.UnicodeTranslatorFromDescriptor("")
+
 	pdf.AddPage()
 
-	r, err := zip.OpenReader("computa.docx")
+	r, err := zip.OpenReader("teste.docx")
 	if err != nil {
 		log.Fatal(err, `err ao ler arquivo`)
 	}
@@ -45,9 +50,6 @@ func main() {
 			}
 			content, err := io.ReadAll(rc)
 			rc.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
 
 			var doc Document
 			err = xml.Unmarshal(content, &doc)
@@ -58,54 +60,60 @@ func main() {
 			for _, para := range doc.Body.Paragraphs {
 				for _, run := range para.Runs {
 
-					if run.Drawing != nil && run.Drawing.Inline != nil {
-						getDrawingId := run.Drawing.Inline.Graphic.Data.Pic.BlipFill.Blip.Embed
-						fmt.Println("Tentando processar imagemID: ", getDrawingId)
-						if nameArq, isRel := relacoes[getDrawingId]; isRel {
-							fmt.Println("Caminho: ", nameArq)
-							if arqZip, existZip := mapaFicheiros[nameArq]; existZip {
-								extensao := filepath.Ext(nameArq)
-								tipoImg := GetImgType(extensao)
-								imgFile, err := arqZip.Open()
-								if err == nil {
-									info := pdf.RegisterImageOptionsReader(nameArq, gofpdf.ImageOptions{ImageType: tipoImg}, imgFile)
-									imgFile.Close()
-									if info == nil {
-										fmt.Println("Não foi possivel ler o nome do arquivo", nameArq)
-									} else {
+					if run.Drawing != nil {
+						var idProcurado string
 
-										_, h := info.Extent()
-										x, y := pdf.GetX(), pdf.GetY()
-										pdf.ImageOptions(nameArq, x, y, 80.0, 0, false, gofpdf.ImageOptions{ImageType: tipoImg}, 0, "")
-										pdf.SetY(y + h + 5)
-									}
-
-								}
-							}
-
+						if run.Drawing.Inline != nil {
+							idProcurado = run.Drawing.Inline.Graphic.Data.Pic.BlipFill.Blip.Embed
+						} else if run.Drawing.Anchor != nil {
+							idProcurado = run.Drawing.Anchor.Graphic.Data.Pic.BlipFill.Blip.Embed
 						}
 
+						if idProcurado != "" {
+							if nameArq, isRel := relacoes[idProcurado]; isRel {
+								if arqZip, existZip := mapaFicheiros[nameArq]; existZip {
+
+									tipoImg := GetImgType(nameArq)
+
+									imgFile, err := arqZip.Open()
+									if err == nil {
+										info := pdf.RegisterImageOptionsReader(nameArq, gofpdf.ImageOptions{ImageType: tipoImg}, imgFile)
+										imgFile.Close()
+
+										if info != nil {
+											_, h := info.Extent()
+											x, y := pdf.GetX(), pdf.GetY()
+
+											if y+h > 275 {
+												pdf.AddPage()
+												y = pdf.GetY()
+											}
+
+											pdf.ImageOptions(nameArq, x, y, 80.0, 0, false, gofpdf.ImageOptions{ImageType: tipoImg}, 0, "")
+											pdf.SetY(y + h + 5)
+										}
+									}
+								}
+							}
+						}
 					}
 
 					style := ""
-
 					if run.Properties.Bold != nil {
 						style += "B"
 					}
-
 					if run.Properties.Italic != nil {
 						style += "I"
 					}
-
 					pdf.SetFont("Arial", style, 12)
 
 					for _, t := range run.Texts {
-						pdf.Write(5, t.Content)
+
+						pdf.Write(5, tr(t.Content))
 					}
 				}
 
 				pdf.Ln(7)
-
 			}
 		}
 	}
@@ -114,6 +122,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println("Escrita terminada....")
+
 }
